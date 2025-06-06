140eb63394692b9818a4d46e7e519c10424404b0
Jeena Paul <email>
2025-06-06 10:41:40 +0530

    Removed finance and added to product

--- ProductDetails/src/About.js
+++
@@ -1,8 +0,0 @@
-import React,{Component} from 'react';
-export default class About extends Component{
-    render(){
-    return(
-    <h3>About: This application provides information about the product</h3>
-    )
-}
-}
--- ProductDetails/src/ProductApi.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default class ProductApi{
-    static getAllProduct(p){
-        axios.get('http://localhost:4000/products')
-        .then(response=>p(response.data))
-        .catch(error=>{throw error});
-    }
-}
